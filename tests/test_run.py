3d4064db33f2d0ea1102badface210cf4e0c12a7
Peter Onyisi <ponyisi@utexas.edu>
2020-12-19 00:15:08 -0600

    Meaningful test

--- tests/test_run.py
+++ tests/test_run.py
@@ -2,9 +2,10 @@ import pytest
 
 def test_run():
     ROOT = pytest.importorskip("ROOT")
-    ROOT.TFile
+    from parquet_to_root import parquet_to_root
+    parquet_to_root('tests/samples/HZZ.parquet', 'HZZ.root', verbose=True)
+    rf = ROOT.TFile.Open('HZZ.root')
+    t = rf.Get('parquettree')
+    assert t.GetEntries() == 2421
     return True
 
-
-# def test_dummy():
-#     return True
