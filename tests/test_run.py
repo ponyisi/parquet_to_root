82124c1970cc0739b9071e0fe22802e0b347afaf
Peter Onyisi <ponyisi@utexas.edu>
2020-12-19 00:33:40 -0600

    Reorder

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,8 +1,8 @@
 import pytest
 
 def test_run():
-    ROOT = pytest.importorskip("ROOT")
     from parquet_to_root import parquet_to_root
+    ROOT = pytest.importorskip("ROOT")
     parquet_to_root('tests/samples/HZZ.parquet', 'HZZ.root', verbose=True)
     rf = ROOT.TFile.Open('HZZ.root')
     t = rf.Get('parquettree')
